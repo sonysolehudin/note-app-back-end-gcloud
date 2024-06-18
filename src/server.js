6d3ecfffcb158116ece151f87c20e4661caa3876
Sony Solehudin <sony.sancakatrain@gmail.com>
2024-06-18 17:42:04 +0700

    Revisi file server.js

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+/*
 const Jwt = require('@hapi/jwt');
 
 // notes
@@ -32,12 +33,15 @@ const CollaborationsValidator = require('./validator/collaborations');
 const _exports = require('./api/exports');
 const ProducerService = require('./services/rabbitmq/ProducerService');
 const ExportsValidator = require('./validator/exports');
+*/
 
 const init = async () => {
+  /*
   const collaborationsService = new CollaborationsService();
   const notesService = new NotesService(collaborationsService);
   const usersService = new UsersService();
   const authenticationsService = new AuthenticationsService();
+  */
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -50,12 +54,15 @@ const init = async () => {
   });
 
   // registrasi plugin eksternal
+  /*
   await server.register([
     {
       plugin: Jwt,
     },
   ]);
+  */
 
+  /*
   // mendefinisikan strategy autentikasi jwt
   server.auth.strategy('notesapp_jwt', 'jwt', {
     keys: process.env.ACCESS_TOKEN_KEY,
@@ -113,6 +120,7 @@ const init = async () => {
       },
     },
   ]);
+  */
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
